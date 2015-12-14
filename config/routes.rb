fef57ea9e0bdec820d19e0f0a40ff9dab3ac08c3
Zaicheng Qi <vmlinz@gmail.com>
2015-12-14 17:42:12 +0800

    Add updating users

--- config/routes.rb
+++ config/routes.rb
@@ -60,7 +60,7 @@ Rails.application.routes.draw do
                               constraints: { subdomain: 'api' }, path: '/' do
     scope module: :v1, constraints: ApiConstraints.new(vesion: 1, default: true) do
     # Resources listed here
-      resources :users, :only => [:show, :create]
+      resources :users, :only => [:show, :create, :update]
     end
   end
 end
