384f046da405561c8062b81e8f452e7105fa373b
Zaicheng Qi <vmlinz@gmail.com>
2015-12-15 00:12:36 +0800

    Fix validation of email uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
+  before_validation :downcase_email
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  private
+    def downcase_email
+      self.email = self.email.downcase if self.email.present?
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  before { @user = FactoryGirl.create :user }
+  before { @user = FactoryGirl.create(:user, :id => 1) }
   subject { @user } 
 
   it { should validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:email) }
+  it { should validate_uniqueness_of(:email).case_insensitive }
   it { should validate_confirmation_of(:password) }
   it { should allow_value('example@domain.com').for(:email) }
 end
