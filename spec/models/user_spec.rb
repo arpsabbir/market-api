a3c27fe424b0b016766ebf73cc2705c4d72e3574
Zaicheng Qi <vmlinz@gmail.com>
2015-12-16 16:26:48 +0800

    Finishing modeling the product with user associations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,13 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   subject(:user) { FactoryGirl.create(:user) }
 
-  it { should respond_to(:auth_token) }
-  it { should validate_presence_of(:email) }
-  it { should validate_uniqueness_of(:email).case_insensitive }
-  it { should validate_confirmation_of(:password) }
-  it { should allow_value('example@domain.com').for(:email) }
-  it { should validate_uniqueness_of(:auth_token) }
+  it { is_expected.to respond_to(:auth_token) }
+  it { is_expected.to validate_presence_of(:email) }
+  it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
+  it { is_expected.to validate_confirmation_of(:password) }
+  it { is_expected.to allow_value('example@domain.com').for(:email) }
+  it { is_expected.to validate_uniqueness_of(:auth_token) }
+  it { is_expected.to have_many :products}
 
   describe "authentication token should work" do
     let!(:user) { FactoryGirl.create :user }
@@ -23,6 +24,21 @@ RSpec.describe User, type: :model do
       existing_user = FactoryGirl.create(:user, auth_token: "auniquetoken123")
       user.generate_authentication_token!
       expect(user.auth_token).not_to eql existing_user.auth_token 
+    end
+  end
+
+  describe "#products association" do
+    before do
+      user.save
+      3.times { FactoryGirl.create :product, user: user}
+    end
+
+    it "destroys the associated products on self destruct" do
+      products = user.products
+      user.destroy
+      products.each do |product|
+        expect(Product.find(product)).to raise_error ActiveRecord::RecordNotFound
+      end
     end
   end
 end
