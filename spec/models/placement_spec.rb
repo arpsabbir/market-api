8c5dc840af410783ff6458b184d564a9272226dc
Zaicheng Qi <vmlinz@gmail.com>
2015-12-21 15:39:12 +0800

    Add quantity to placements

--- spec/models/placement_spec.rb
+++ spec/models/placement_spec.rb
@@ -6,6 +6,7 @@ RSpec.describe Placement, type: :model do
 
   it { is_expected.to respond_to :order_id }
   it { is_expected.to respond_to :product_id }
+  it { is_expected.to respond_to :quantity }
   it { is_expected.to belong_to :order }
   it { is_expected.to belong_to :product }
 end
